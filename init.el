bd51a9367792fea9a5bcc75689f2915094f30b99
Stupin Sergey <stupin.sergey@gmail.com>
2013-03-17 23:57:47 +0400

    rm redundant bars

--- init.el
+++ init.el
@@ -3,3 +3,8 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 (delete-selection-mode t)
+
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
