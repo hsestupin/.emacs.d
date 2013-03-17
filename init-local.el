2afa971858f0af946759140535b7af06d927e29a
Stupin Sergey <stupin.sergey@gmail.com>
2013-03-18 03:12:40 +0400

    fullscreen hook

--- init-local.el
+++ init-local.el
@@ -7,4 +7,7 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
+;; set fullscreen mode on startup  
+(set-frame-parameter nil 'fullscreen 'fullboth)
+
 (provide 'init-local)
