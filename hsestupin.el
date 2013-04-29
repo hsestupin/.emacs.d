4069836d31012a89792703bb495f5d78038c9d12
Stupin Sergey <stupin.sergey@gmail.com>
2013-04-29 23:09:17 +0400

    add clojurescript support

--- hsestupin.el
+++ hsestupin.el
@@ -13,3 +13,6 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'none)
 (global-set-key (kbd "M-`") 'ns-next-frame)
+
+;; add clojruescript support
+(add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
