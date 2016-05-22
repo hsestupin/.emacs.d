10dcebfda3c99e84997cf0860d7f1bf2666015a5
hsestupin <stupin.sergey@gmail.com>
2016-05-23 00:09:05 +0300

    delete unused cidet-start-http-server definition

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -2,6 +2,10 @@
 ;; Clojure
 ;;;;
 
+
+;; Enable eldoc-mode
+(add-hook 'clojure-mode-hook 'eldoc-mode)
+
 ;; Enable paredit for Clojure
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
 
@@ -29,9 +33,6 @@
 ;; Cider
 ;;;;
 
-;; provides minibuffer documentation for the code you're typing into the repl
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-
 ;; go right to the REPL buffer when it's finished connecting
 (setq cider-repl-pop-to-buffer-on-connect t)
 
@@ -55,17 +56,6 @@
 (add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
 
 
-;; key bindings
-;; these help me out with the way I usually develop web apps
-(defun cider-start-http-server ()
-  (interactive)
-  (cider-load-current-buffer)
-  (let ((ns (cider-current-ns)))
-    (cider-repl-set-ns ns)
-    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
-    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
-
-
 (defun cider-refresh ()
   (interactive)
   (cider-interactive-eval (format "(user/reset)")))
@@ -76,7 +66,7 @@
 
 (eval-after-load 'cider
   '(progn
-     (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
      (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
      (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
      (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
+
