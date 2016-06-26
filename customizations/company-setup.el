b1d8148dbc473dcb3a71323de9b9ae2c3661e575
hsestupin <stupin.sergey@gmail.com>
2016-06-26 15:52:41 +0300

    align annotations to right + fix load error

--- customizations/company-setup.el
+++ customizations/company-setup.el
@@ -2,18 +2,23 @@
 
 (global-set-key (kbd "C-SPC") #'company-complete)
 
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-(setq company-idle-delay 0.5)
+(with-eval-after-load 'company	  
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
 
-(custom-set-faces
- `(company-tooltip ((t :background "lightgray" :foreground "black")))
- `(company-tooltip-selection ((t :background "steelblue" :foreground "white")))
- `(company-tooltip-mouse ((t :background "blue" :foreground "white")))
- `(company-tooltip-common ((t :background "lightgray" :foreground "black")))
- `(company-tooltip-common-selection ((t t :background "lightgray" :foreground "black")))
- ;; `(company-tooltip-annotation ((t :background "" :foreground "")))
- `(company-scrollbar-fg ((t :background "black")))
- `(company-scrollbar-bg ((t :background "gray")))
- `(company-preview ((t :background nil :foreround "darkgray")))
- `(company-preview-common ((t :background nil :foreground "darkgray"))))
+  (setq company-idle-delay 0.5)
+
+  (custom-set-variables
+   `(company-tooltip-align-annotations t))
+  
+  (custom-set-faces
+   `(company-tooltip ((t :background "lightgray" :foreground "black")))
+   `(company-tooltip-selection ((t :background "steelblue" :foreground "white")))
+   `(company-tooltip-mouse ((t :background "blue" :foreground "white")))
+   `(company-tooltip-common ((t :background "lightgray" :foreground "black")))
+   `(company-tooltip-common-selection ((t t :background "lightgray" :foreground "black")))
+   ;; `(company-tooltip-annotation ((t :background "" :foreground "")))
+   `(company-scrollbar-fg ((t :background "black")))
+   `(company-scrollbar-bg ((t :background "gray")))
+   `(company-preview ((t :background nil :foreround "darkgray")))
+   `(company-preview-common ((t :background nil :foreground "darkgray")))))
