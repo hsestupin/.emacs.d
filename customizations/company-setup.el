ac8fe5827fb8c1797b5f5ab505560361f45e55a1
hsestupin <stupin.sergey@gmail.com>
2016-06-26 14:41:54 +0300

    add C-n C-p nagivation keys in company completions

--- customizations/company-setup.el
+++ customizations/company-setup.el
@@ -1,3 +1,6 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 (global-set-key (kbd "C-SPC") #'company-complete)
+
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
